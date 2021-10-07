93a94b4271355f5361df07d0b9ddb2400248aa98
unho <djsgh94@naver.com>
2021-10-07 18:02:23 +0900

    codetest

--- CodeTest/Python/BaekJoon/16236.py
+++ CodeTest/Python/BaekJoon/16236.py
@@ -4,21 +4,36 @@ from pprint import pprint
 sys.stdin = open('input.txt')
 
 
-def bfs(start):
-    q = deque()
+def bfs(i, j):
+    global time, eat_cnt, shark_size, shark_coordinate
 
-    visited = [[0] * N for _ in range(N)]       # 물고기 먹으러 가는 길 체크 (한번 먹을때마다 길을 되돌아갈수있으므로 초기화)
-    visited[start[0]][start[1]] = 1
+    q = deque()
+    q.append((i, j))
+    visited = [[0] * N for _ in range(N)]
+    visited[i][j] = 1
 
     while q:
         node = q.popleft()
+        
         for k in range(4):
-            r = start[0] + dr[k]
-            c = start[1] + dc[k]
+            r = node[0] + dr[k]
+            c = node[1] + dc[k]
 
-            if 0 <= r < N and 0 <= c < N and shark_size >= sea[r][c] and not visited[r][c]:
+            if 0 <= r < N and 0 <= c < N and not visited[r][c] and sea[r][c] <= shark_size:
                 visited[r][c] = visited[node[0]][node[1]] + 1
+                if sea[r][c] and shark_size > sea[r][c]:
+                    sea[r][c] = 0
+                    sea[shark_coordinate[0]][shark_coordinate[1]] = 0
+                    shark_coordinate = [r, c]
+                    eat_cnt += 1
+                    if eat_cnt == shark_size:
+                        shark_size += 1
+                        eat_cnt = 0
+                    time += (visited[r][c] - 1)
+                    return True
 
+                q.append((r, c))
+    return False
 
 
 dr = [-1, 0, 1, 0]      # 가장 위에 있는
@@ -26,19 +41,22 @@ dc = [0, -1, 0, 1]      # 가장 왼쪽에 있는
 
 N = int(sys.stdin.readline())
 sea = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-fish_info = {}          # 물고기들 정보 - 키: 크기 / 값 : 위치 좌표
+fish_cnt = [0] * 7      # 사이즈별 남은 상어 수
+shark_coordinate = []   # 상어 위치
+eat_cnt = 0             # 상어가 먹은 횟수
 shark_size = 2          # 상어 사이즈
-time = 0
+time = 0                # 흐른 시간
 
-for i in range(N):
+for i in range(N):                          # 바다에서 물고기와 상어의 정보를 정리함
     for j in range(N):
-        if sea[i][j]:
-            fish_info[sea[i][j]] = fish_info.get(sea[i][j], []) + [(i, j)]
+        if sea[i][j] == 9:
+            shark_coordinate = [i, j]
+        elif sea[i][j]:
+            fish_cnt[sea[i][j]] += 1
 
-# print('----- fish_info -----')
-# pprint(fish_info)
+while True:       # 상어보다 작은 크기의 물고기가 있다면
+    if not bfs(shark_coordinate[0], shark_coordinate[1]):
+        break
 
-while shark_size > min(fish_info.keys()):
-    pass
-
+print(sea)
 print(time)
