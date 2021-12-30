5fc2ac28c3a0d4a5829417ea2fa2dd06789d7ff8
unho <djsgh94@naver.com>
2021-12-30 14:00:48 +0900

    codetest

--- CodeTest/Python/BaekJoon/0_7662_다시풀어보기.py
+++ CodeTest/Python/BaekJoon/0_7662_다시풀어보기.py
@@ -1,51 +1,44 @@
-'''
-시간초과
-'''
-
 import sys
-from collections import deque
+import heapq
 sys.stdin = open('input.txt')
 
 
-T = int(sys.stdin.readline())
+T = int(sys.stdin.readline())                                   # 테스트 케이스 개수
 
 for _ in range(T):
-    K = int(sys.stdin.readline())
-    low_q = deque([])
-    high_q = deque([])
+    K = int(sys.stdin.readline())                               # 명령의 개수
+    min_heap, max_heap = [], []                                 # 최소힙, 최대힙으로 사용할 리스트
+    deleted = [0] * (K+1)                                       # 삭제된 숫자들 목록 리스트 (0-삭제되지 않음, 1-삭제됨)
 
-    for _ in range(K):
-        command, num = sys.stdin.readline().split()
+    for i in range(K):
+        command, num = sys.stdin.readline().split()             # 명령어 종류, 숫자
         num = int(num)
 
-        if command == 'I':
-            while True:
-                if low_q and low_q[-1] > num:
-                    high_q.append(low_q.pop())
-                elif high_q and high_q[-1] < num:
-                    low_q.append(high_q.pop())
-                else:
-                    low_q.append(num)
-                    break
+        if command == 'I':                                      # 삽입 연산
+            heapq.heappush(min_heap, (num, i))                  # 최소힙과 최대힙에 모두 추가
+            heapq.heappush(max_heap, (-num, i))                        
 
-        elif command == 'D':
-            if num == -1:
-                if low_q:
-                    low_q.popleft()
-                elif high_q:
-                    high_q.pop()
-            elif num == 1:
-                if high_q:
-                    high_q.popleft()
-                elif low_q:
-                    low_q.pop()
+        elif command == 'D' and max_heap:                       # 삭제 연산
+            if num == -1:                                       # 최소힙 삭제인 경우
+                while min_heap and deleted[min_heap[0][1]]:     # 최소힙에 값이 남아있고, 현재 최솟값의 인덱스가 최대힙에서 삭제된 인덱스이면 삭제
+                    heapq.heappop(min_heap)
+                if min_heap:                                    # 여전히 최소힙이 남아있으면
+                    node = heapq.heappop(min_heap)              # 최솟값 삭제
+                    deleted[node[1]] = 1                        # 삭제한 최솟값의 인덱스를 삭제 처리
 
+            else:                                               # 최대힙 삭제인 경우
+                while max_heap and deleted[max_heap[0][1]]:     # 최대힙에 값이 남아있고, 현재 최댓값의 인덱스가 최소힙에서 삭제된 인덱스이면 삭제
+                    heapq.heappop(max_heap)                             
+                if max_heap:                                    # 최대힙이 남아있으면
+                    node = heapq.heappop(max_heap)              # 최댓값 삭제 및 방금 삭제한 최댓값의 인덱스 삭제 처리
+                    deleted[node[1]] = 1
 
-    if not low_q and not high_q:
+    while min_heap and deleted[min_heap[0][1]]:                 # 최소힙에서 최솟값이 최대힙에서 삭제됬으면 삭제
+        heapq.heappop(min_heap)                                
+    while max_heap and deleted[max_heap[0][1]]:                 # 최대힙에서 최댓값이 최소힙에서 삭제됬으면 삭제
+        heapq.heappop(max_heap)
+
+    if not min_heap:                                            # 출력
         print('EMPTY')
-    elif not low_q:
-        print(high_q[0], high_q[-1])
-    elif not high_q:
-        print(low_q[-1], low_q[0])
     else:
-        print(high_q[0], low_q[0])
+        print(-heapq.heappop(max_heap)[0], heapq.heappop(min_heap)[0])
