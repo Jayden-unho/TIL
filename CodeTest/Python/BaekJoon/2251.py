c29f53f25a358b3e1d47d29bf47f7d213b7bafe7
unho-00 <djsgh94@naver.com>
2022-06-16 01:58:16 +0900

    [codetest] 2251 modify

--- CodeTest/Python/BaekJoon/2251.py
+++ CodeTest/Python/BaekJoon/2251.py
@@ -1,32 +1,29 @@
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
 
-def solution(case):
-    stack = case
-    water = [0, 0, buckets[2]]
+def solution():
+    q = deque([(0, 0)])                 # 처음 A, B 물통은 비어있음
 
-    # print('------------------------')
-    # print(f'start : {water}')
-    while stack:
-        t1, t2 = stack.pop()
-        # print(f't1 : {t1} | t2 : {t2}')
-        can_move = min(buckets[t1], buckets[t2], water[t1])
-        water[t2] += can_move
-        water[t1] -= can_move
-        # print(f'move : {can_move} | water : {water}')
+    while q:
+        a, b = q.popleft()              # 현재 A, B 바구니 물의 양
+        c = C - a - b                   # C 바구니 물의 양
 
-    if not water[0]:
-        answer.add(water[2])
-    
-    
-buckets = list(map(int, sys.stdin.readline().split()))
-answer = {buckets[2]}
+        if not a:                       # A 바구니가 비어있다면, 정답에 추가
+            answer.add(c)
+        
+        for element in [(a + min(c, A-a), b), (a, b + min(c, B-b)),         # C -> A, C -> B 바구니로 물을 옮김
+            (a + min(b, A-a), b - min(b, A-a)), (a, b - min(b, C-c)),       # B -> A, B -> C 바구니로 물을 옮김
+            (a - min(a, B-b), b + min(a, B-b)), (a - min(a, C-c), b)]:      # A -> B, A -> C 바구니로 물을 옮김
+            
+            if not visited.get((element[0], element[1]), False):            # A 바구니와 B 바구니에 남은 물의 양을 탐색하지 않았으면
+                visited[(element[0], element[1])] = True                    # 탐색 처리
+                q.append((element[0], element[1]))                          # 큐에 추가
+        
+A, B, C = map(int, sys.stdin.readline().split())        # 물통의 최대양
+answer = set()                                          # 정답의 집합
+visited = {}                                            # A, B 물통에 현재 담겨있는 물의 양을 탐색했는지 여부
 
-for case in [[[2, 1]], [[0, 1], [2, 0]], [[0, 2], [2, 1], [2, 0]], [[0, 1], [1, 2], [2, 0], [2, 1]], [[0, 2], [1, 0], [2, 1]]]:
-    solution(case)
+solution()
 
-print(*sorted(answer))
-# 1 C->B                            | C->B
-# 2 C->A & A->B                     | C->A->B
-# 3 C->A & C->B & A->C              | C->A, C->B, A->C
-# 4 C->B & C->A & B->C & A->C       | C->B, C->A, B->C, A->B
+print(*sorted(answer))              # 오름차순 출력
