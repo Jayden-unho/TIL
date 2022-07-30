3704b719554c5f73b1829c6f5e2529e02929e968
unho-00 <djsgh94@naver.com>
2022-07-30 13:46:23 +0900

    [codetest] python

--- CodeTest/Python/BaekJoon/1504.py
+++ CodeTest/Python/BaekJoon/1504.py
@@ -2,19 +2,17 @@ import sys
 import heapq
 sys.stdin = open('input.txt')
 
-
 def solution(start: int):                           # 다익스트라 탐색
-    distance = [-1] * (N+1)                         # 출발지로부터 각 노드까지 거리
+    distance = [1e10] * (N+1)                         # 출발지로부터 각 노드까지 거리
     heap = [(0, start)]                             # 최소힙 리스트, 출발점 세팅
-
+    
     while heap:
-        node = heapq.heappop(heap)
-        if distance[node[1]] < 0:                   # 아직 안가본 길이면, 거기까지 가는 최소 거리 저장
-            distance[node[1]] = node[0]
-            for e in linked[node[1]]:
-                heapq.heappush(heap, (distance[node[1]] + e[0], e[1]))
+        dist, node = heapq.heappop(heap)
+        if distance[node] > dist:                   # 아직 안가본 길이면, 거기까지 가는 최소 거리 저장
+            distance[node] = dist
+            for next in linked[node]:
+                heapq.heappush(heap, (distance[node] + next[0], next[1]))
     return distance
-
 
 N, E = map(int, sys.stdin.readline().split())           # 정점의 개수, 간선의 개수
 linked = [[] for _ in range(N+1)]                       # 정점들 간에 연결 관계
@@ -26,13 +24,16 @@ for _ in range(E):
 
 necessary = list(map(int, sys.stdin.readline().split()))    # 꼭 방문해야할 정점 리스트
 
-dis_init = solution(1)                      # 1번 정점에서 각 정점으로의 거리 구함
+dis_init = solution(1)                                      # 1번 정점에서 각 정점으로의 거리 구함
 
-if dis_init[necessary[0]] >= 0 and dis_init[necessary[1]] >= 0:     # 꼭 방문해야하는 두 정점을 갈 수 있다면
-    dis_1 = solution(necessary[0])          # 꼭 방문할 첫번째 정점에서 다른 정점으로의 거리
-    dis_2 = solution(necessary[1])          # 두번쨰 정점에서 다른 정점으로의 거리
+dis_1 = solution(necessary[0])                              # 꼭 방문할 첫번째 정점에서 다른 정점으로의 거리
+dis_2 = solution(necessary[1])                              # 두번쨰 정점에서 다른 정점으로의 거리
 
-    # 1 -> 정점1 -> 정점2 -> N 과 1 -> 정점2 -> 정점1 -> N 으로 가는 두가지 방법 중 더 짧은 거리 출력
-    print(min(dis_init[necessary[0]] + dis_1[necessary[1]] + dis_2[N], dis_init[necessary[1]] + dis_2[necessary[0]] + dis_1[N]))
-else:
+# 출발지 -> n1 -> n2 -> 도착지와 출발지 -> n2 -> n1 -> 도착지 중 짧은 거리
+ans = min(dis_init[necessary[0]] + dis_1[necessary[1]] + dis_2[N], dis_init[necessary[1]] + dis_2[necessary[0]] + dis_1[N])
+
+# 경로 존재하지 않는다면 -1, 그 외 출력
+if ans >= 1e10:
     print(-1)
+else:
+    print(ans)
